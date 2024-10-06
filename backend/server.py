0410c7a3761d0827c0b60f0c723eaa3dfec3b1ca
sundaram123krishnan <krishnsundaram@gmail.com>
2024-10-06 20:38:30 +0530

    chore: add cors

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 model = joblib.load('./suspicious_model.pkl')
 
